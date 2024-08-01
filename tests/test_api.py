3059d32f2e4e61706e825e9204392802f37a0ea4
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-08-01 19:46:45 +0900

    Update testing

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,4 @@
-def test_root(test_client):
+def test_healthchecker(test_client):
     response = test_client.get("/api/v1/healthchecker")
     assert response.status_code == 200
     assert response.json() == {"message": "The API is LIVE!!"}
