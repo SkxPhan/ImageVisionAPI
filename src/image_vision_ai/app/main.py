c81eb93c0575b3903a54af8a6b56b2d728300a59
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-08-03 21:14:20 +0900

    Update default port

--- src/image_vision_ai/app/main.py
+++ src/image_vision_ai/app/main.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         reload=True,
         log_config="log.ini",
     )
