b5341fcd386bebdd078e84c527ae9324bde98dab
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-08-17 11:23:21 +0900

    Add integration tests for auth

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -1,8 +1,40 @@
+from datetime import timedelta
+
 import bcrypt
 import pytest
+from fastapi import HTTPException
 
 import app.models as models
 from app.database import TokenBlacklistORM
+from app.routers.auth import (
+    create_access_token,
+    get_current_user,
+    get_password_hash,
+    get_user,
+)
+
+
+@pytest.mark.integration
+def test_get_user(user_payload, db_session):
+    # inject user in db
+    hashed_password = get_password_hash(user_payload["password"])
+    new_user = models.UserORM(
+        username=user_payload["username"],
+        email=user_payload["email"],
+        hashed_password=hashed_password,
+    )
+    db_session.add(new_user)
+    db_session.commit()
+
+    # retrieve the user
+    user = get_user(user_payload["username"], db_session)
+    assert user
+    assert user.username == user_payload["username"]
+    assert user.email == user_payload["email"]
+
+    # retrieve inexistent user
+    user = get_user("inexistent_user", db_session)
+    assert user is None
 
 
 @pytest.mark.api
@@ -100,3 +132,55 @@ def test_logout(
         .first()
     )
     assert blacklisted_token is not None
+
+
+@pytest.mark.api
+@pytest.mark.integration
+def test_get_current_user(
+    test_client,
+    user_payload,
+    register_endpoint,
+    login_endpoint,
+    logout_endpoint,
+    db_session,
+):
+    response = test_client.post(
+        register_endpoint,
+        json=user_payload,
+    )
+    response = test_client.post(
+        login_endpoint,
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+        data={
+            "grant_type": "password",
+            "username": user_payload["username"],
+            "password": user_payload["password"],
+        },
+    )
+    # Test Case 1: Valid token
+    access_token = response.json()["access_token"]
+    user = get_current_user(access_token, db_session)
+    assert user is not None
+    assert user.username == user_payload["username"]
+    assert user.email == user_payload["email"]
+
+    # Test Case 2: Other user's token
+    bad_access_token = create_access_token(data={"sub": "unauthorized_user"})
+    with pytest.raises(HTTPException):
+        user = get_current_user(bad_access_token, db_session)
+
+    # Test Case 3: Expired token
+    expired_token = create_access_token(
+        data={"sub": user_payload["username"]},
+        expires_delta=timedelta(minutes=-1),
+    )
+    with pytest.raises(HTTPException):
+        user = get_current_user(expired_token, db_session)
+
+    # Test Case 4: Blacklisted token
+    response = test_client.post(
+        logout_endpoint,
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    with pytest.raises(HTTPException):
+        user = get_current_user(access_token, db_session)
---
+++ tests/unit/test_auth.py
@@ -0,0 +1,29 @@
+import bcrypt
+import pytest
+
+from app.routers.auth import get_password_hash, verify_password
+
+
+@pytest.mark.unit
+def test_get_password_hash():
+    password = "my_secret_password"
+    hashed_password = get_password_hash(password)
+    assert isinstance(hashed_password, str)
+    assert hashed_password != password
+    bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+
+@pytest.mark.unit
+def test_verify_password():
+    # Test Case 1: Correct password
+    plain_password = "my_secret_password"
+    hashed_password = get_password_hash(plain_password)
+    assert verify_password(plain_password, hashed_password) is True
+
+    # Test Case 2: Incorrect password
+    wrong_password = "wrong_password"
+    assert verify_password(wrong_password, hashed_password) is False
+
+    # Test Case 3: Empty password
+    empty_password = ""
+    assert verify_password(empty_password, hashed_password) is False
