73cd4330e188fab2da24b798879df127e66ac00b
SkxPhan <110777139+SkxPhan@users.noreply.github.com>
2024-08-22 11:32:20 +0900

    Refactoring unit test

--- tests/unit/test_cnn.py
+++ tests/unit/test_cnn.py
@@ -4,8 +4,8 @@ import torch
 from app.ml.cnn_model import ImageClassifier, Preprocessor
 
 
-@pytest.fixture
-def mock_image_classifier(monkeypatch):
+@pytest.fixture(scope="function")
+def mocked_image_classifier(monkeypatch):
     def mock_init(self, *args, **kwargs):
         self._categories = ["cat", "dog", "car", "plane", "boat"]
 
@@ -15,6 +15,8 @@ def mock_image_classifier(monkeypatch):
         return torch.tensor([0.0098, 0.0494, 0.8500, 0.0788, 0.012])
 
     monkeypatch.setattr(ImageClassifier, "predict", mock_predict)
+
+    return ImageClassifier()
 
 
 @pytest.mark.unit
@@ -26,9 +28,8 @@ def test_preprocessor(image):
 
 
 @pytest.mark.unit
-def test_top_k_predictions(mock_image_classifier, image):
-    image_classifier = ImageClassifier()
-    predictions = image_classifier.top_k_predictions(image, 3)
+def test_top_k_predictions(mocked_image_classifier, image):
+    predictions = mocked_image_classifier.top_k_predictions(image, 3)
     assert len(predictions) == 3
 
     expected_prediction = [("car", 0.8500), ("plane", 0.0788), ("dog", 0.0494)]
@@ -40,23 +41,21 @@ def test_top_k_predictions(mock_image_classifier, image):
 
 
 @pytest.mark.unit
-def test_predict_category(mock_image_classifier, image):
-    image_classifier = ImageClassifier()
-    prediction = image_classifier.predict_category(image)
+def test_predict_category(mocked_image_classifier, image):
+    prediction = mocked_image_classifier.predict_category(image)
     label, prob = prediction
     assert label == "car"
     assert prob == pytest.approx(0.8500, rel=1e-3)
 
 
 @pytest.mark.unit
-def test_fail_predict_category(mock_image_classifier, image, monkeypatch):
+def test_fail_predict_category(mocked_image_classifier, image, monkeypatch):
     def mock_predict(*args, **kwargs):
         return torch.tensor([0.5, 0.5])
 
-    monkeypatch.setattr(ImageClassifier, "predict", mock_predict)
+    monkeypatch.setattr(mocked_image_classifier, "predict", mock_predict)
 
-    image_classifier = ImageClassifier()
-    prediction = image_classifier.predict_category(image)
+    prediction = mocked_image_classifier.predict_category(image)
     label, prob = prediction
     assert label == "Unknown"
     assert prob is None
